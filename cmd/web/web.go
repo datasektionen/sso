4bf72af4317db778c11bfd695cf32e73e5a42fd1
Viktor Ekby <viktoe@datasektionen.se>
2025-12-04 20:52:25 +0100

    feat: add manual account requests

--- cmd/web/web.go
+++ cmd/web/web.go
@@ -57,7 +57,7 @@ func serve(s *service.Service, op http.Handler, internal bool, p int) {
 	if internal {
 		started = "Internal server started"
 	}
-	slog.Info(started, "address", "http://localhost:"+port, "config", config.Config)
+	slog.Info(started, "address", "http://localhost:"+port)
 	slog.Error("Failed serving http server", "error", http.Serve(l, mux))
 	os.Exit(1)
 }
